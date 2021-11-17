15587923e38b9acf592fd8837730a60d0c241646
Timon Horlboge <timon@horlboge.de>
2021-11-17 14:26:31 +0100

    updated regex

--- main.py
+++ main.py
@@ -17,9 +17,11 @@ def prepare():
     cleanup()
     shutil.copytree("server", "temp")
 
+
 def cleanup():
     if os.path.exists("temp"):
         shutil.rmtree("temp")
+
 
 def saveWorld(name):
     chdir_base()
@@ -30,11 +32,12 @@ def saveWorld(name):
 
 def runServerAndGenerate(radius=1500):
     os.chdir("temp")
-    child = pexpect.spawn('java -Xms2048M -Xmx2048M -jar server.jar nogui')
+    child = pexpect.spawn('java -Xms2048M -Xmx4096M -jar server.jar --nojline --log-strip-color --nogui')
     child.logfile = sys.stdout.buffer
     child.expect('Done \(', timeout=None)
     child.sendline('fcp start %i world' % radius)
-    child.expect('\[100\.0%\]', timeout=None)
+    child.expect(r'Generating\sChunks:\s(\d+)\sof\s\1',
+                 timeout=None)  # regex is ugly, but works.
     child.sendline('stop')
     child.expect('Stopping server', timeout=None)
     child.expect(pexpect.EOF, timeout=None)
@@ -47,6 +50,13 @@ def generateWorld(name, radius=1500):
     saveWorld(name)
 
 
-generateWorld("world2", 750)
-generateWorld("world3", 500)
+generateWorld("world2", 2000)
+generateWorld("world3", 2000)
+generateWorld("world4", 2000)
+generateWorld("world5", 2000)
+generateWorld("world6", 2000)
+generateWorld("world7", 2000)
+generateWorld("world8", 2000)
+generateWorld("world9", 2000)
+generateWorld("world10", 2000)
 cleanup()
